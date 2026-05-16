8cee6e74ab2bc41516bb35381375f47f5fb3a4aa
vladiukdaria-debug <vladiukdaria@gmail.com>
2026-05-16 23:32:17 +0200

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#" \
              "-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
